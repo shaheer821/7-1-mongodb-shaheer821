879344238316aef750de1711281100cfc9d71991
shaheer971 <shaheerahmar4@gmail.com>
2025-11-18 16:23:23 +0300

    todo2

--- 7-1-mongodb/server.js
+++ 7-1-mongodb/server.js
@@ -185,6 +185,14 @@ connectDB();
 
 
 // define schema
+const studentSchema = new mongoose.Schema({
+   name: String,
+   age: Number,
+   major: String
+});
+
+const Student = mongoose.model("Student", studentSchema);
+
 
 
 // create document
