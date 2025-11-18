00dcc9daa5b5acce9c9900aed916d6a6d2468386
shaheer971 <shaheerahmar4@gmail.com>
2025-11-18 16:30:57 +0300

    todo5

--- 7-1-mongodb/server.js
+++ 7-1-mongodb/server.js
@@ -217,6 +217,12 @@ readStudents();
 
 
 // update document
+async function updateStudent() {
+   await Student.updateOne({ name: "Ali" }, { age: 22 });
+   console.log("✅ Updated Ali");
+}
+
+updateStudent();
 
 
 // delete document
